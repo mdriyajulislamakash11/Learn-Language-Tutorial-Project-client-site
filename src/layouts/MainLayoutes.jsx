a5996a6e03bd37d22bb84fd0f2f8708380b34e8a
Akash <hafejmohammadakash@gmail.com>
2025-07-07 15:54:07 +0600

    navbar er kaj coltese

--- src/layouts/MainLayoutes.jsx
+++ src/layouts/MainLayoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { Outlet } from 'react-router-dom'
 
 const MainLayoutes = () => {
   return (
@@ -11,13 +12,13 @@ const MainLayoutes = () => {
 
         {/* dynamic Layoute */}
         <div>
-
+            <Outlet />
         </div>
 
         {/* Footer */}
         <Footer />
 
-        
+
     </div>
   )
 }
