69f5911435dc0c0eb629983d4ba730545d7d0cc4
Akash <hafejmohammadakash@gmail.com>
2025-07-13 11:24:15 +0600

    state er kaj holo

--- src/components/StateSection.jsx
+++ src/components/StateSection.jsx
@@ -4,6 +4,11 @@ const StateSection = () => {
   return (
     <div>
       <h2 className="text-center text-4xl font-bold ">Learning Overview</h2>
+      <p className="w-2/4 mx-auto text-center my-4">
+        Interactively benchmark value-added infrastructures without ethical
+        solutions. Progressively incentivize cutting-edge data whereas
+        plug-and-play convergence. Uniquely.
+      </p>
       <div className=" mt-10 flex justify-between items-center text-center">
         {/* heading */}
 
@@ -19,7 +24,7 @@ const StateSection = () => {
           <div className="stat-desc">5-star tutor reviews</div>
         </div>
 
-        {/* all totors */}on the App Store
+        {/* all totors */}
         <div className="stat">
           <div className="stat-value">4,200</div>
           <div className="stat-desc">Subjects taught</div>
