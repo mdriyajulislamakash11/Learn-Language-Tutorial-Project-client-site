66cf4897b1a209caae908da518434f2513a08e27
Akash <hafejmohammadakash@gmail.com>
2025-07-13 11:07:10 +0600

    state er kaj onk tai hoye gese ekhon backend theke data ante hobe

--- src/components/StateSection.jsx
+++ src/components/StateSection.jsx
@@ -3,36 +3,32 @@ import React from "react";
 const StateSection = () => {
   return (
     <div>
-      <h2 className="">Learning Overview</h2>
-      <div className="border-2 border-red-500 flex justify-between items-center text-center">
+      <h2 className="text-center text-4xl font-bold ">Learning Overview</h2>
+      <div className=" mt-10 flex justify-between items-center text-center">
         {/* heading */}
 
         {/* all totors */}
         <div className="stat">
-          <div className="stat-title">New Users</div>
           <div className="stat-value">4,200</div>
-          <div className="stat-desc">↗︎ 400 (22%)</div>
+          <div className="stat-desc">Experienced tutors</div>
         </div>
 
         {/* all totors */}
         <div className="stat">
-          <div className="stat-title">New Users</div>
           <div className="stat-value">4,200</div>
-          <div className="stat-desc">↗︎ 400 (22%)</div>
+          <div className="stat-desc">5-star tutor reviews</div>
+        </div>
+
+        {/* all totors */}on the App Store
+        <div className="stat">
+          <div className="stat-value">4,200</div>
+          <div className="stat-desc">Subjects taught</div>
         </div>
 
         {/* all totors */}
         <div className="stat">
-          <div className="stat-title">New Users</div>
           <div className="stat-value">4,200</div>
-          <div className="stat-desc">↗︎ 400 (22%)</div>
-        </div>
-
-        {/* all totors */}
-        <div className="stat">
-          <div className="stat-title">New Users</div>
-          <div className="stat-value">4,200</div>
-          <div className="stat-desc">↗︎ 400 (22%)</div>
+          <div className="stat-desc">on the App Store</div>
         </div>
       </div>
     </div>
