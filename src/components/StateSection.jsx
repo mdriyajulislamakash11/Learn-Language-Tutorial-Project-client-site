3a68888095a8d5e20db29eb1470ad78f576a5542
Akash <hafejmohammadakash@gmail.com>
2025-07-13 10:48:02 +0600

    state er kaj suru korlam

---
+++ src/components/StateSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const StateSection = () => {
+    return (
+        <div>
+            {/* all viwer sate */}
+
+
+
+            {/* all reviww state */}
+
+
+
+            {/* all language state */}
+
+
+
+            {/* all user state */}
+
+
+
+        </div>
+    );
+};
+
+export default StateSection;
